9353df14bb6ba38a7c8f3f15e35c9f340999c736
Tommaso Romani <44830726+TommasoRomani@users.noreply.github.com>
2021-06-08 17:38:25 +0200

    iniziato a scrivere codice
    robe a casp

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Dio Dio"
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class DogsittingAppointment(db.model):
+    prenotation_number = db.column(db.Integer, primary_key=True)
+    dogsitter_name = db.column(db.String(), nullable=False)
+    dog_number = db.Column(db.Integer, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 if __name__ == "__main__":
-    app.run(debug=True):
+    app.run(debug=True)
