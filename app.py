99d0dbc0ea61ecbafe47cff89ed55f758fd82d1d
Tommaso Romani <44830726+TommasoRomani@users.noreply.github.com>
2021-06-10 14:05:03 +0200

    Da sistemare errore 400
    Problema con richiesta post e tipo datetime in SQLAlchemy

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_restful import Api, Resource, marshal_with, reqparse, abort, fields
+from flask_restful import Api, Resource, marshal_with, reqparse, abort, fields, inputs
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,11 +11,38 @@ user_put_parser.add_argument("name", type=str, help="Nome dell'utente non inseri
 user_put_parser.add_argument("email", type=str, help="Email non inserita")
 user_put_parser.add_argument("passwd", type=str, help="Password non inserita")
 
+
+schedule_put_parser = reqparse.RequestParser()
+schedule_put_parser.add_argument("dogsitter_id", type=int)
+schedule_put_parser.add_argument("location", type=str)
+#schedule_put_parser.add_argument("date_start", type=lambda x: datetime.strptime(x,'%Y-%m-%dT%H:%M'))
+#schedule_put_parser.add_argument("date_end", type=lambda x: datetime.strptime(x,'%Y-%m-%dT%H:%M'))
+schedule_put_parser.add_argument("date_start", type=str)
+schedule_put_parser.add_argument("date_end", type=str)
+schedule_put_parser.add_argument("max_dogs", type=int)
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-resource_fields = {
+
+@app.route('/appointment')
+def appointment():
+    return render_template('appointment.html')
+
+
+resource_fields_schedules = {
+    'appointment_id': fields.Integer,
+    'dogsitter_id': fields.Integer,
+    'location': fields.String,
+    'appointment_start': fields.DateTime,
+    'appointment_end': fields.DateTime,
+    'max_dog_number': fields.Integer
+}
+
+
+resource_fields_users = {
     'id': fields.Integer,
     'email': fields.String,
     'username': fields.String,
@@ -23,9 +50,49 @@ resource_fields = {
     'user_type':  fields.Boolean
 }
 
+
+class AddSchedule(Resource):
+    def post(self):
+        request = schedule_put_parser.parse_args()
+        print(request['date_start'])
+        tempo = datetime.strptime(request['date_start'],'%Y-%m-%dT%H:%M')
+        print(tempo)
+
+        #controlla se la lista è vuote e inizializza il primo elemento id a 0
+        if not AvailableDogsitter.query.all():
+            schedule = AvailableDogsitter(appointment_id=0,location=request['location'], appointment_start=request['date_start'], appointment_end=request['date_end'], max_dog_number=request['max_dogs'])
+            try:
+                db.session.add(schedule)
+                db.session.commit()
+                return 200
+            except:
+                return 'Non è stato possibile inserire i dati del primo utente'
+        #se la lista non è vuota tutti gli id successivi saranno impostati ad il valore dell'id precedente aumentato di 1 
+        else:
+            max_id = db.session.query(db.func.max(AvailableDogsitter.appointment_id)).scalar()
+            schedule = AvailableDogsitter(appointment_id=(max_id+1),location=request['location'], appointment_start=request['date_start'], appointment_end=request['date_end'], max_dog_number=request['max_dogs'])
+            try:
+                db.session.add(schedule)
+                db.session.commit()
+                return 200
+            except:
+                return 'Non è stato possibile inserire i dati'
+
+
+class ScheduleList(Resource):
+    @marshal_with(resource_fields_schedules)
+    def get(self):
+        try:
+            tmp = AvailableDogsitter.query.all()
+            return tmp
+        except:
+            return {"message":"Query non effettuata correttamente"}, 404
+
+
 class AddUsers(Resource):
     def post(self):
         request = user_put_parser.parse_args()
+
         #controlla se la lista è vuote e inizializza il primo elemento id a 0
         if not UserTable.query.all():
             users = UserTable(id=0,email=request['email'], username=request['name'], password=request['passwd'], user_type=request['user_type'])
@@ -48,7 +115,7 @@ class AddUsers(Resource):
                 
 
 class User(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(resource_fields_users)
     def get(self, user_id):
         try:
             return UserTable.query.filter_by(id=user_id).first()
@@ -57,7 +124,7 @@ class User(Resource):
 
 
 class UsersList(Resource):
-    @marshal_with(resource_fields)
+    @marshal_with(resource_fields_users)
     def get(self):
         try:
             tmp = UserTable.query.all()
@@ -69,6 +136,8 @@ class UsersList(Resource):
 api.add_resource(AddUsers, "/api/add_users")
 api.add_resource(UsersList, "/api/users")
 api.add_resource(User, "/api/user/<int:user_id>")
+api.add_resource(AddSchedule, "/api/add_schedule")
+api.add_resource(ScheduleList, "/api/schedule_list")
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -79,15 +148,23 @@ class UserTable(db.Model):
     __tablename__= 'usertable'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(), nullable=False, unique=True)
-    username = db.Column(db.String(), nullable=False)
-    password = db.Column(db.String(), nullable=False)
+    username = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     user_type = db.Column(db.Boolean, nullable=False)#0 per utenti base, 1 per dogsitter
 
+class AvailableDogsitter(db.Model):
+    __tablename__='availabledogsitter'
+    appointment_id = db.Column(db.Integer, primary_key=True)
+    dogsitter_id = db.Column(db.Integer, db.ForeignKey('usertable.id'))
+    location = db.Column(db.String(200), nullable=False)
+    max_dog_number = db.Column(db.Integer, nullable=False)
+    appointment_start = db.Column(db.DateTime, nullable=False)
+    appointment_end = db.Column(db.DateTime, nullable=False)
 
 class DogsittingAppointment(db.Model):
     __tablename__ = 'dogsittingappointment'
     prenotation_number = db.Column(db.Integer, primary_key=True)
-    dogsitter_name = db.Column(db.String(), nullable=False)
+    dogsitter_name = db.Column(db.String(200), nullable=False)
     dog_number = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
     userId = db.Column(db.Integer, db.ForeignKey('usertable.id'))
